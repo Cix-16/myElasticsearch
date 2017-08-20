0932921255fe41791455d16eb1ff9cc7ebcd2206
Cix-16 <chenxiaoxiao1992@126.com>
2017-08-20 18:51:36 +0800

    enviroment

---
+++ src/main/java/spdb/bigdata/myElasticsearch/App.java
@@ -0,0 +1,13 @@
+package spdb.bigdata.myElasticsearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
